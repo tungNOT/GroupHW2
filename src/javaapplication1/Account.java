2b27077f59d41efa69e67b1a4da5392c4616d4a4
PC200 <PC200@134.154.40.66>
2019-03-13 10:27:14 -0700

    first commit

---
+++ src/javaapplication1/Account.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author PC200
+ */
+public class Account {
+    private String accountName;
+    private String industry;
+    private String billingCity;
+    private String billingState;
+    private int phone;
+    private String accountOwner;
+    private int numOfEmployee;
+    private List<Contact> contactLst;
+    
+    public Account(String a_accName, String a_indus, String a_billCity, String a_billState, int a_phone, String a_owner) {
+        accountName = a_accName;
+        industry = a_indus;
+        billingCity = a_billCity;
+        billingState = a_billState;
+        phone = a_phone;
+        accountOwner = a_owner;
+        contactLst = new ArrayList<>();
+    }
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public String getBillingCity() {
+        return billingCity;
+    }
+
+    public void setBillingCity(String billingCity) {
+        this.billingCity = billingCity;
+    }
+
+    public String getBillingState() {
+        return billingState;
+    }
+
+    public void setBillingState(String billingState) {
+        this.billingState = billingState;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getAccountOwner() {
+        return accountOwner;
+    }
+
+    public void setAccountOwner(String accountOwner) {
+        this.accountOwner = accountOwner;
+    }
+
+    public int getNumOfEmployee() {
+        return numOfEmployee;
+    }
+
+    public void setNumOfEmployee(int numOfEmployee) {
+        this.numOfEmployee = numOfEmployee;
+    }
+
+    public List<Contact> getContactLst() {
+        return contactLst;
+    }
+    
+    public void newContact (String a_name, String a_title, int a_phone, String a_email){
+        Contact newContact = new Contact(a_name, a_title, a_phone, a_email);
+        contactLst.add(newContact);
+    }
+    
+}
