29a2eb221c49508e8aeb3bcca704c19e5aa8195a
PC200 <PC200@134.154.40.66>
2019-03-13 10:25:00 -0700

    first commit

---
+++ src/javaapplication1/Contact.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author PC200
+ */
+public class Contact {
+    private String name;
+    private String title;
+    private int phone;
+    private String email;
+
+    public Contact(String name, String title, int phone, String email) {
+        this.name = name;
+        this.title = title;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
