5d13b8c452ef9752824476c9e82f5b344353d954
tungNOT <48067768+tungNOT@users.noreply.github.com>
2019-03-29 22:20:24 -0700

    Add files via upload

---
+++ src/javaapplication1/Call_Log (1).java
@@ -0,0 +1,17 @@
+
+package javaapplication1;
+
+import java.util.ArrayList;
+
+
+public class Call_Log {
+    
+    public ArrayList<String> LOG_LIST;
+    
+     public Call_Log()
+     {
+         LOG_LIST=new ArrayList<>();
+     }
+    
+     
+}
